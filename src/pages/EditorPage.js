8c51e1e064407a510d836d8846397fab3b015f94
Golden23twig <sonalsingh3214@gmail.com>
2025-09-21 17:39:47 +0530

    Add full project

---
+++ src/pages/EditorPage.js
@@ -0,0 +1,166 @@
+import React, { useState, useRef, useEffect } from "react";
+import toast from "react-hot-toast";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
+import { language, cmtheme } from "../../src/atoms";
+import { useRecoilState } from "recoil";
+import ACTIONS from "../actions/Actions";
+import { initSocket } from "../socket";
+import {
+  useLocation,
+  useNavigate,
+  Navigate,
+  useParams,
+} from "react-router-dom";
+
+const EditorPage = () => {
+  const [lang, setLang] = useRecoilState(language);
+  const [them, setThem] = useRecoilState(cmtheme);
+
+  const [clients, setClients] = useState([]);
+
+  const socketRef = useRef(null);
+  const codeRef = useRef(null);
+  const location = useLocation();
+  const { roomId } = useParams();
+  const reactNavigator = useNavigate();
+
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed, try again later.");
+        reactNavigator("/");
+      }
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+
+      // Listening for joined event
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+          setClients(clients);
+          socketRef.current.emit(ACTIONS.SYNC_CODE, {
+            code: codeRef.current,
+            socketId,
+          });
+        }
+      );
+
+      // Listening for disconnected
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+        toast.success(`${username} left the room.`);
+        setClients((prev) => {
+          return prev.filter((client) => client.socketId !== socketId);
+        });
+      });
+    };
+    init();
+    return () => {
+      socketRef.current.off(ACTIONS.JOINED);
+      socketRef.current.off(ACTIONS.DISCONNECTED);
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  async function copyRoomId() {
+    try {
+      await navigator.clipboard.writeText(roomId);
+      toast.success("Room ID has been copied to clipboard");
+    } catch (err) {
+      toast.error("Could not copy the Room ID");
+      console.error(err);
+    }
+  }
+
+  function leaveRoom() {
+    reactNavigator("/");
+  }
+
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/logo.png" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+
+        <label>
+          Select Language:
+          <select
+            value={lang}
+            onChange={(e) => {
+              setLang(e.target.value);
+              window.location.reload();
+            }}
+            className="seLang"
+          >
+            <option value="clike">C / C++ / C# / Java</option>
+            <option value="css">CSS</option>
+            <option value="dart">Dart</option>
+            <option value="django">Django</option>
+            <option value="dockerfile">Dockerfile</option>
+            <option value="go">Go</option>
+            <option value="htmlmixed">HTML-mixed</option>
+            <option value="javascript">JavaScript</option>
+            <option value="jsx">JSX</option>
+            <option value="markdown">Markdown</option>
+            <option value="php">PHP</option>
+            <option value="python">Python</option>
+            <option value="r">R</option>
+            <option value="rust">Rust</option>
+            <option value="ruby">Ruby</option>
+            <option value="sass">Sass</option>
+            <option value="shell">Shell</option>
+            <option value="sql">SQL</option>
+            <option value="swift">Swift</option>
+            <option value="xml">XML</option>
+            <option value="yaml">yaml</option>
+          </select>
+        </label>
+
+        <button className="btn copyBtn" onClick={copyRoomId}>
+          Copy ROOM ID
+        </button>
+        <button className="btn leaveBtn" onClick={leaveRoom}>
+          Leave
+        </button>
+      </div>
+
+      <div className="editorWrap">
+        <Editor
+          socketRef={socketRef}
+          roomId={roomId}
+          onCodeChange={(code) => {
+            console.log("on code change" + code);
+            codeRef.current = code;
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default EditorPage;
